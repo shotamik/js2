3194f8c9f7e17032890ecc592ccb2e06090afef9
shotamik <shotamikeladze@gmail.com>
2022-10-25 23:21:22 +0400

    added third task

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 
 const myArr = [10, 20, 30, 40, 50];
 
+console.log(myArr);
+
 console.log((myArr[0] + myArr[1] + myArr[2] + myArr[3] + myArr[4]) / 5);
 
 const myArr1 = [
@@ -30,3 +32,16 @@ const myArr1 = [
 ]
 
 console.log(myArr1);
+
+let obj1 = {
+    firstName: "Shota",
+    lastName: "Mikeladze",
+    age: 20,
+    phoneNumbers: [555543545, 545346364, 599654567],
+	address: "Vazha-Pshavela Street",
+	zipCode: 123,
+
+}
+
+
+console.log(`user name is ${obj1.firstName}, user age is ${obj1.age}, user address is ${obj1.address}`);
