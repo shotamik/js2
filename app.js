f98eaf3a3e37c34c547f4f463772100914a6a6d9
shotamik <shotamikeladze@gmail.com>
2022-10-25 23:18:52 +0400

    added second task

--- app.js
+++ app.js
@@ -5,3 +5,28 @@
 const myArr = [10, 20, 30, 40, 50];
 
 console.log((myArr[0] + myArr[1] + myArr[2] + myArr[3] + myArr[4]) / 5);
+
+const myArr1 = [
+    {
+        name: "Shota",
+        age: 21
+    },
+    {
+        name: "Shota",
+        age: 21
+    },
+    {
+        name: "Shota",
+        age: 21
+    },
+    {
+        name: "Shota",
+        age: 21
+    },
+    {
+        name: "Shota",
+        age: 21
+    }
+]
+
+console.log(myArr1);
